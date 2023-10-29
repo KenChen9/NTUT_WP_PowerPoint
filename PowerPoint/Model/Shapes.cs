6013670cb08344c1d45192d566223e8c82d90c13
KenChen <ken27281@gmail.com>
2023-10-29 16:21:45 +0800

    完成Shapes的實作

--- PowerPoint/Model/Shapes.cs
+++ PowerPoint/Model/Shapes.cs
@@ -8,6 +8,21 @@ namespace PowerPoint
 {
     public class Shapes
     {
+        private List<Shape> _shapes = new List<Shape>();
 
+        public void Add(ShapeType shapeType)
+        {
+            _shapes.Add(Factory.CreateShape(shapeType));
+        }
+
+        public void RemoveAt(int index)
+        {
+            _shapes.RemoveAt(index);
+        }
+
+        public void Draw(IGraphics graphics)
+        {
+            _shapes.ForEach(shape => shape.Draw(graphics));
+        }
     }
 }
