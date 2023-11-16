8fce8271e5c37d7a1bb65402b4f4c8d47ac2a5b8
KenChen <ken27281@gmail.com>
2023-11-17 02:29:26 +0800

    移除Enum的註解

--- PowerPoint/Model/Enum/ShapeColor.cs
+++ PowerPoint/Model/Enum/ShapeColor.cs
@@ -1,18 +1,8 @@
 ﻿namespace PowerPoint
 {
-    /// <summary>
-    /// Enum representing different colors for shapes in a PowerPoint presentation.
-    /// </summary>
     public enum ShapeColor
     {
-        /// <summary>
-        /// Black color for shapes.
-        /// </summary>
         Black,
-
-        /// <summary>
-        /// Red color for shapes.
-        /// </summary>
         Red
     }
 }
--- PowerPoint/Model/Enum/ShapeType.cs
+++ PowerPoint/Model/Enum/ShapeType.cs
@@ -1,28 +1,10 @@
 ﻿namespace PowerPoint
 {
-    /// <summary>
-    /// Enum representing different types of shapes in a PowerPoint presentation.
-    /// </summary>
     public enum ShapeType
     {
-        /// <summary>
-        /// Arrow shape.
-        /// </summary>
         Arrow,
-
-        /// <summary>
-        /// Line shape.
-        /// </summary>
         Line,
-
-        /// <summary>
-        /// Rectangle shape.
-        /// </summary>
         Rectangle,
-
-        /// <summary>
-        /// Circle shape.
-        /// </summary>
         Circle
     }
 }
